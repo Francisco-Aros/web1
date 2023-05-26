6a91e568fa33fcd122af529bc711605872c47355
Francisco-Aros <franciscoarosmunoz@gmail.com>
2023-05-26 10:56:05 -0400

    cambios en texto

--- Acerca_de.py
+++ Acerca_de.py
@@ -118,7 +118,7 @@ def main():
     st.markdown(
             """
             <div class="footer">
-                <p>Página web creada por Francisco Javier Aros Muñoz.</p>
+                <p>Página web diseñada y desarrollada por Francisco Javier Aros Muñoz.</p>
                 <a href="mailto:franciscoarosmunoz@gmail.com">franciscoarosmunoz@gmail.com</a>
             </div>
             """,
