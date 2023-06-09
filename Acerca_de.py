1070e8acb870cff40787bed49d48d2d0b9d80505
Francisco-Aros <franciscoarosmunoz@gmail.com>
2023-06-09 13:47:23 -0400

    config

--- Acerca_de.py
+++ Acerca_de.py
@@ -7,7 +7,7 @@ import streamlit.components.v1 as components
    #Configuración inicial de la página
 st.set_page_config(
     page_title="Mi Aplicación Personalizada",
-    page_icon=":round_pushpin:",
+    page_icon=":globe_with_meridians:",
     layout="wide"
 )
 
